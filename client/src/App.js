3b34cfbbebc8f5ee247810b8751ed5dce743a257
grunc10161122 <raunaksandhu10@gmail.com>
2023-11-28 15:56:59 -0700

    app fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,3 @@
-import React, { useState, useEffect } from "react";
-
-function App() {
-  const [data, setData] = useState([{}]);
-  const [UsersNames, setUsersNames] = useState([{}]);
-
-//   useEffect(() => {
-//     fetch("/members")
-//       .then((res) => res.json())
-//       .then((data) => {
-//         setData(data);
-//         console.log(data);
-//       });
-//   }, []);
-
-  useEffect(() => {
-    fetch("/User_fname")
-      .then((res) => res.json())
-      .then((data) => {
-        setUsersNames(data);
-        console.log(data);
-      });
-  }, []);
-
-//   return (
-//     <div>
-//       <h1>Member List</h1>
-//       <ul>
-//         {typeof data.members === "undefined" ? (
-//           <p>loading...</p>
-//         ) : (
-//           data.members.map((member, index) => <li key={index}>{member}</li>)
-//         )}
-//       </ul>
-
-//       {/*------------------------ */}
-//       <ul>
-//         {typeof UsersNames.User_fname === "undefined" ? (
-//           <p>loading...</p>
-//         ) : (
-//           UsersNames.User_fname.map((fname, index) => (
-//             <li key={index}>{fname[0]}</li>
-//           ))
-//         )}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import UserDashboard from './components/UserDashboard';
@@ -60,6 +7,7 @@ import UserSignup from './components/UserSignup';
 const App = () => {
   return (
     <Router>
+      
 
       <Routes> 
         <Route path="/" element={<Login />} />
@@ -68,28 +16,6 @@ const App = () => {
         <Route path="/admin_dashboard" element={<AdminDashboard />} />
       </Routes>
     </Router>
-    <div>
-      <h1>Member List</h1>
-      <ul>
-        {typeof data.members === "undefined" ? (
-          <p>loading...</p>
-        ) : (
-          data.members.map((member, index) => <li key={index}>{member}</li>)
-        )}
-      </ul>
-
-      {/*------------------------ */}
-      <ul>
-        {typeof UsersNames.User_fname === "undefined" ? (
-          <p>loading...</p>
-        ) : (
-          UsersNames.User_fname.map((fname, index) => (
-            <li key={index}>{fname[0]}</li>
-          ))
-        )}
-      </ul>
-    </div>
   );
-}
-
+};
 export default App;
