d77e79c48d769b6e3ff4fdcbfe7530cdf025f88a
grunc10161122 <raunaksandhu10@gmail.com>
2023-11-30 02:31:37 -0700

    updates to dashboards

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -1,17 +1,77 @@
 import React from 'react';
 import Banner from './Banner';
-
 import { useState } from 'react';
 
+const AdminDashboard = ({ email, handleLogout }) => {
+  // State to manage the visibility of different sections
+  const [showOrders, setShowOrders] = useState(false);
+  const [showClientDonations, setShowClientDonations] = useState(false);
+  const [showAddAdmin, setShowAddAdmin] = useState(false);
+  const [showFulfillOrders, setShowFulfillOrders] = useState(false);
+  const [showInventory, setShowInventory] = useState(false);
 
-const AdminDashboard = ({email, handleLogout}) => {
   return (
     <div>
       <Banner />
       <div>
         <h2>Admin Dashboard</h2>
+        <button onClick={() => setShowOrders(!showOrders)}>Order from Supplier</button>
+        <button onClick={() => setShowClientDonations(!showClientDonations)}>Add Client Donation</button>
+        <button onClick={() => setShowAddAdmin(!showAddAdmin)}>Add Admin Account</button>
+        <button onClick={() => setShowFulfillOrders(!showFulfillOrders)}>Fulfill/View Requests</button>
+        <button onClick={() => setShowInventory(!showInventory)}>View Inventory</button>
+        <button onClick={handleLogout}>Logout</button>
       </div>
-      <button onClick={handleLogout}>Logout</button>
+      {showOrders && <OrdersSection />}
+      {showClientDonations && <ClientDonationsSection />}
+      {showAddAdmin && <AddAdminSection />}
+      {showFulfillOrders && <FulfillOrdersSection />}
+      {showInventory && <InventorySection />}
+    </div>
+  );
+};
+
+// Example section components (you can replace these with your actual components)
+const OrdersSection = () => {
+  return (
+    <div>
+      <h3>Orders Section</h3>
+      {/* Add content for viewing orders */}
+    </div>
+  );
+};
+
+const ClientDonationsSection = () => {
+  return (
+    <div>
+      <h3>Client Donations Section</h3>
+      {/* Add content for viewing client donations */}
+    </div>
+  );
+};
+
+const AddAdminSection = () => {
+  return (
+    <div>
+      <h3>Add Admin Account Section</h3>
+      {/* Add content for adding admin accounts */}
+    </div>
+  );
+};
+
+const FulfillOrdersSection = () => {
+  return (
+    <div>
+      <h3>Fulfill Orders Section</h3>
+      {/* Add content for fulfilling orders */}
+    </div>
+  );
+};
+
+const InventorySection = () => {
+  return (
+    <div>
+      <h3>Inventory Section</h3>
     </div>
   );
 };
--- client/src/components/UserDashboard.js
+++ client/src/components/UserDashboard.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Banner from './Banner';
-import { useState } from 'react';
+import axios from 'axios';
 
-const UserDashboard = ({email, handleLogout}) => {
+const UserDashboard = ({ email, handleLogout }) => {
+  const [date, setDate] = useState('');
+  const [userType, setUserType] = useState('');
 
-  const [date, setDate] = useState('')
-  const [ch_email, setEmail] = useState(email);
+  const [showMakeRequest, setShowMakeRequest] = useState(false);
+  const [showDonateItems, setShowDonateItems] = useState(false);
+  const [showViewInventory, setShowViewInventory] = useState(false);
 
+  useEffect(() => {
+    // Fetch user type from Flask backend using Axios
+    const fetchUserType = async () => {
+      try {
+        const response = await axios.post('/getUsertype', { email }); // Replace '/getUsertype' with your actual endpoint
+        setUserType(response.data.usertype);
+      } catch (error) {
+        console.error('Error fetching user type:', error);
+      }
+    };
+
+    fetchUserType();
+  }, [email]); // Fetch user type when the component is loaded and email changes
 
   const handleDateChange = (event) => {
     setDate(event.target.value);
-    console.log(event.target.value);
-    console.log(ch_email);
-  }
+  };
+
+  const handleMakeRequestClick = () => {
+    setShowMakeRequest(true);
+    setShowDonateItems(false);
+    setShowViewInventory(false);
+  };
+
+  const handleDonateItemsClick = () => {
+    setShowMakeRequest(false);
+    setShowDonateItems(true);
+    setShowViewInventory(false);
+  };
+
+  const handleViewInventoryClick = () => {
+    setShowMakeRequest(false);
+    setShowDonateItems(false);
+    setShowViewInventory(true);
+  };
 
   return (
     <div>
@@ -20,7 +52,7 @@ const UserDashboard = ({email, handleLogout}) => {
       <div>
         <h2>Hamper Request</h2>
 
-        <p>Select your preferred pickup date for your pickup: </p>
+        <p>Select your preferred pickup date for your pickup:</p>
         <input type="date" value={date} onChange={handleDateChange}></input>
 
         <p>
@@ -33,13 +65,51 @@ const UserDashboard = ({email, handleLogout}) => {
 
         <p>
           The following items are running low or missing, so be aware they might not be
-          included in your hamper: </p>
-          
+          included in your hamper:
+        </p>
         <p label="low/missing"></p>
       </div>
+
+      <button onClick={handleMakeRequestClick}>Make Request</button>
+      <button onClick={handleDonateItemsClick}>Donate Items</button>
+      <button onClick={handleViewInventoryClick}>View Inventory</button>
+
+      {showMakeRequest && <MakeRequest email={email} />}
+      {showDonateItems && <DonateItems email={email} />}
+      {showViewInventory && <ViewInventory />}
+      
       <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
+
+const MakeRequest = ({ email }) => {
+  return (
+    <div>
+      <h3>Make Request Component</h3>
+      {/* Add content for making a request */}
+    </div>
+  );
+};
+
+
+const DonateItems = ({ email }) => {
+  return (
+    <div>
+      <h3>Donate Items Component</h3>
+      {/* Add content for donating items */}
+    </div>
+  );
+};
+
+const ViewInventory = () => {
+  return (
+    <div>
+      <h3>View Inventory Component</h3>
+      {/* Add content for viewing inventory */}
+    </div>
+  );
+};
+
 
 export default UserDashboard;
