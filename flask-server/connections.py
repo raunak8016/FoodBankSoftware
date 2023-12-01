8233dc4effee67c73545b646b7fe0231282582c1
DaveOGQ <otigeorgedavid@gmail.com>
2023-12-01 00:33:56 -0700

    updated db name to foodbankdb

--- flask-server/connections.py
+++ flask-server/connections.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 app.config['MySQL_HOST'] = 'localhost' #default localhost port:5000
 app.config["MYSQL_USER"] = "root"   #username for MySQL database
 app.config["MYSQL_PASSWORD"] = "LoweMe124" #David's password
-app.config["MYSQL_DB"] = "foodBank"    #name of the database
+app.config["MYSQL_DB"] = "foodbankdb"    #name of the database
 
 mysql = MySQL(app)      #connect MySql to the app
